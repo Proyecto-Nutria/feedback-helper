f19f3924ee9842043c168651dfa2ae3d0b5ca529
searleser97 <gabrielsanv97@gmail.com>
2020-08-26 11:57:33 -0500

    production ready

--- src/Index.bs.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-var React = require("react");
-var ReactDOMRe = require("reason-react/src/legacy/ReactDOMRe.bs.js");
-var FeedbackHelper$FeedbackHelper = require("./FeedbackHelper/FeedbackHelper.bs.js");
-
-ReactDOMRe.renderToElementWithId(React.createElement(FeedbackHelper$FeedbackHelper.make, {}), "root");
-
-/*  Not a pure module */
