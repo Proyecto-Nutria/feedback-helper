fd283de27c98685304b99972d60bd0336c3e1b69
searleser97 <gabrielsanv97@gmail.com>
2020-09-04 18:19:24 -0500

    improve workflow

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,18 @@
 const path = require("path")
 const HtmlWebpackPlugin = require("html-webpack-plugin")
+const webpack = require("webpack");
 const outputDir = path.join(__dirname, ".")
+const PORT = process.env.PORT || 8000;
+const URL = `http://localhost:${PORT}/`;
+const subdomain = 'feedback-helper';
+
+const pathRewrite = {}
+pathRewrite['^/feedback-helper'] = '';
+const proxy = {}
+proxy[`/${subdomain}/*`] = {
+  target: URL,
+  pathRewrite: pathRewrite
+};
 
 module.exports = {
   entry: "./src/Index.bs.js",
@@ -13,16 +25,20 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: "index.html",
-      inject: true
+      inject: false
+    }),
+    new webpack.DefinePlugin({
+      'process.env.URL': JSON.stringify(process.env.URL || `${URL}${subdomain}`)
     })
   ],
   devServer: {
-    publicPath: 'http://localshot:8000/feedback-helper',
+    publicPath: `/${subdomain}`,
     compress: true,
     contentBase: outputDir,
-    port: process.env.PORT || 8000,
+    port: PORT,
     historyApiFallback: true,
-    host: "0.0.0.0"
+    host: "0.0.0.0",
+    proxy: proxy
   },
   module: {
     rules: [
